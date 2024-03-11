d5971aa10d1c2e4c96b5092018f261c51b367c2d
SohyangO <shyang903@naver.com>
2024-03-11 19:19:43 +0900

    Update:CSR 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 // src>app>rendering/page.tsx
-import SSR from "../components/rendering/SSR";
+import ISR from "../components/rendering/ISR";
 import React from "react";
 
 const RenderingTestPage = () => {
   return (
     <div>
       <h1>4가지 렌더링 방식을 테스트합니다.</h1>
-      <SSR />
+      <ISR />
     </div>
   );
 };
