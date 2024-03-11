38fe74c7dce6eda3fb96ff21cc9a2e4431f85cbf
SohyangO <shyang903@naver.com>
2024-03-11 15:49:01 +0900

    Add: SSG 실습해보기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+// src>app>rendering/page.tsx
+import SSG from "../components/rendering/SSG";
+import React from "react";
 
-export default function Home() {
-  return <div>메인 페이지입니다.</div>;
-}
+const RenderingTestPage = () => {
+  return (
+    <div>
+      <h1>4가지 렌더링 방식을 테스트합니다.</h1>
+      <SSG />
+    </div>
+  );
+};
+
+export default RenderingTestPage;
